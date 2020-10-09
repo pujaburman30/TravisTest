2cc2174a287d4290fd17f9256cb2e9118419db95
Someshbose <iamsomeshbose@gmail.com>
2020-10-09 21:10:30 +0530

    checkstyle changes

--- src/main/java/main/AddTwoNumbers.java
+++ src/main/java/main/AddTwoNumbers.java
@@ -1,7 +1,21 @@
 package main;
 
-public class AddTwoNumbers {
-	public static int add(int a,int b) {
-		return a+b;
-	}
+/**
+ * Class for adding two numbers.
+ * 
+ * @author somesh
+ *
+ */
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public final class AddTwoNumbers {
+  /**
+   * Static Method to add two numbers.
+   * 
+   * @param a int
+   * @param b int
+   * @return int
+   */
+  public static int add(int a, int b) {
+    return a + b;
+  }
 }
